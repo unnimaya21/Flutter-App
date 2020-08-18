fb48b160ebc3d44c0b52705e41768a69f99a1adb
unnimaya21 <68774089+unnimaya21@users.noreply.github.com>
2020-08-18 15:41:26 +0530

    grn

---
+++ lib/grn.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+//import 'package:pjt/prinlist.dart';
+import 'package:pjt/gridprin.dart';
+
+class GrnReport extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Menu"),
+        ),
+        body: Container(),
+        //   )
+        // ]),
+      ),
+    );
+  }
+}
